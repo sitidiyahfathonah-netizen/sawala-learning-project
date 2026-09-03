9e58256228d7775acb9e554553d431e07ec01a81
sitidiyahfathonah-netizen <sitidiyahfathonah@gmail.com>
2026-09-03 10:11:59 +0700

    variabel data

---
+++ 01-javascript-fundamentals/01-variabel-tipe-data/index.js
@@ -0,0 +1,50 @@
+const nama = "Budi";
+const umur = 17
+const sudahMenikah = false;
+const tinggiBadan = 170;
+const hobi = "berenang";
+
+console.log(nama);
+console.log(umur);
+console.log(sudahMenikah);
+console.log(tinggiBadan);
+console.log(hobi);
+
+console.log(typeof (nama));
+console.log(typeof (umur));
+console.log(typeof (sudahMenikah));
+console.log(typeof (tinggiBadan));
+console.log(typeof (hobi));
+
+let kota = "Jakarta";
+
+console.log(kota);
+
+kota = "Bandung";
+
+console.log(kota);
+
+let alamat;
+const nomorTelepon = null;
+
+console.log(alamat);
+console.log(typeof alamat);
+
+console.log(nomorTelepon);
+console.log(typeof nomorTelepon);
+
+
+const angka1 = 20;
+const angka2 = 6;
+
+const penjumlahan = angka1 + angka2;
+const pengurangan = angka1 - angka2;
+const perkalian = angka1 * angka2;
+const pembagian = angka1 / angka2;
+const sisaPembagian = angka1 % angka2;
+
+console.log("Penjumlahan:", penjumlahan);
+console.log("Pengurangan:", pengurangan);
+console.log("Perkalian:", perkalian);
+console.log("Pembagian:", pembagian);
+console.log("Sisa Pembagian:", sisaPembagian);
